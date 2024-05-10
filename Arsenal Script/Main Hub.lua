2cb3171292ecfeeacde00ae1c4e4f69fa752e34f
ImNotSkidoLOL <pavel.y.smirnov@yandex.ru>
2024-05-11 02:52:02 +0300

    Update Main Hub.lua

--- Arsenal Script/Main Hub.lua
+++ Arsenal Script/Main Hub.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ShaddowScripts/Main/main/Library"))()
+local library = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/SkidoLOL/ArsenalScript/main/Arsenal%20Script/Ui%20Lib/Shadow%20Lib.lua"))()
 
 local Main = library:CreateWindow("Main","Crimson")
 
