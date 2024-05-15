dc5872c075304f964d1edc918e4404bf882e95f9
ImNotSkidoLOL <pavel.y.smirnov@yandex.ru>
2024-05-15 23:33:36 +0300

    Update GunSettings.lua

--- Arsenal Script/Scripts For MainHub/GunSettings.lua
+++ Arsenal Script/Scripts For MainHub/GunSettings.lua
@@ -23,3 +23,9 @@ for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
        v.Value = 20
    end
 end
+
+--Inf Ammo
+while wait() do
+	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
+	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
+end
