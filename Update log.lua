9340cd7c77d7a0fc9eddcea13b85db3a4aeb8b2c
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2024-05-17 09:51:28 +0300

    Update Update log.lua

--- Update log.lua
+++ Update log.lua
@@ -1,12 +1,1 @@
---[[
-10.05.24
-1. made script safe to use
-2. Maybe I will update library soon
-3. Script Is Still Beta
-11.05.24
-1.Added Speed Bypass
-2.Updated Lib
-3.Added AutoMod-ban Bypasser
-4.Added Rejoin Server Button
-5.Added Fly Gui
-]]--
+--ALL LOG IN DISCORD FOR NOW!
