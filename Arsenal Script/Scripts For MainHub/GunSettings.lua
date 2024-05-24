c3cff7898f9c28e80fac880c4fdb849afe918205
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2024-05-25 00:35:05 +0300

    Update GunSettings.lua

--- Arsenal Script/Scripts For MainHub/GunSettings.lua
+++ Arsenal Script/Scripts For MainHub/GunSettings.lua
@@ -19,13 +19,4 @@ for i, v in pairs(replicationstorage.Weapons:GetDescendants()) do
    if v.Name == "FireRate" then
        v.Value = 0.05
    end
-   if v.Name == "Crit" then
-       v.Value = 20
-   end
-end
-
---Inf Ammo
-while wait() do
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount.Value = 999
-	game:GetService("Players").LocalPlayer.PlayerGui.GUI.Client.Variables.ammocount2.Value = 999
 end
