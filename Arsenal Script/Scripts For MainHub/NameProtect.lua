660dfa8b9b55cb6714146afcef71da0a65ebfa24
Qwiz <125666833+qqwizzixxxx@users.noreply.github.com>
2024-06-09 18:04:11 +0300

    Update NameProtect.lua

--- Arsenal Script/Scripts For MainHub/NameProtect.lua
+++ Arsenal Script/Scripts For MainHub/NameProtect.lua
@@ -1,4 +1,5 @@
 while wait() do
+    game.Players.LocalPlayer.PlayerGui.GUI_Scorecard.Scorecard.Scrolling.Visible = false
     game.Workspace.KillFeed["1"].Killer.Value = "discord.gg/qkWVWph6ck"
     game.Workspace.KillFeed["2"].Killer.Value = "discord.gg/qkWVWph6ck"
     game.Workspace.KillFeed["3"].Killer.Value = "discord.gg/qkWVWph6ck"
@@ -7,4 +8,5 @@ while wait() do
     game.Workspace.KillFeed["6"].Killer.Value = "discord.gg/qkWVWph6ck"
     game.Players.LocalPlayer.PlayerGui.GUI.Winner.Visible = false
     game.Players.LocalPlayer.PlayerGui.Menew_Main.Container.PlrName.Text = "discord.gg/qkWVWph6ck"
+    game.Players.LocalPlayer.PlayerGui.GUI_Scorecard.Scorecard.PlayerCard.Username.Text = "qqwizzixxxx >:)"
 end
